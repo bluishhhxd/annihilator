4c5f2f3ad16633c7931820a7ea60649f2ae69fd2
Bluishhh <145332477+bluishhhxd@users.noreply.github.com>
2023-11-22 01:36:46 +0530

    Update annnihilator.py

--- annnihilator.py
+++ annnihilator.py
@@ -1,6 +1,7 @@
 import os
 import hashlib
 from collections import defaultdict
+import time
 
 
 def compute_hash(file_path):
@@ -29,7 +30,6 @@ def find_duplicate_files(directory_to_scan):
 
 def delete_duplicates(duplicate_files):
     for files in duplicate_files:
-        # Keep the first file, delete the rest
         for file_path in files[1:]:
             try:
                 os.remove(file_path)
@@ -39,25 +39,59 @@ def delete_duplicates(duplicate_files):
 
 
 if __name__ == "__main__":
-    directory_to_scan = input("Enter the directory path to scan: ")
-
-    if not os.path.isdir(directory_to_scan):
-        print("Invalid directory path.")
-    else:
-        duplicate_files = find_duplicate_files(directory_to_scan)
-
-        if not duplicate_files:
-            print("No duplicate files found.")
+    print("\n\nWelcome to project ANNIHILATE.\nIt is specifically designed to delete duplicate files from your Pc/laptop.\nEnjoy your free space :)\n\n\n1.) Scan: Scans for duplicate files in the provided directory path.\n2.) Quit: Quits the program")
+    while True: 
+        button = input("\nType your choice:\n").replace(" ",'')
+        if button.lower() == 'scan':
+            pass
+        elif button.lower() == 'quit':
+            print("\nThank you for using our Annihilator.\n\n\n")
+            break
+        elif button == '':
+            print("\nYou can't leave it empty\nIf you wish to exit the program type" + ' "quit".')
+            continue
         else:
-            print("Duplicate files found:")
-            for files in duplicate_files:
-                for file_path in files:
-                    print(file_path)
+            print("\nInvalid option, Try again.")
+            continue
+        while True:
+            directory_to_scan = input("\nEnter the directory path to scan for duplicate files: ")
 
-            confirmation = input("Delete duplicates? (yes/no): ").strip().lower()
-
-            if confirmation == 'yes':
-                delete_duplicates(duplicate_files)
-                print("Duplicates deleted.")
+            if directory_to_scan.capitalize() == "Quit" :
+                print("\n\nOkay, Thank you for using our Annihilator.\n\n\n")
+                break
+            elif directory_to_scan == "" :
+                print("\nTry again, Directory path can't be empty.")
+            elif not os.path.isdir(directory_to_scan):
+                print("\nPlease enter a valid directory path.")
             else:
-                print("Duplicates not deleted.")
+                duplicate_files = find_duplicate_files(directory_to_scan)
+
+                if not duplicate_files:
+                    print("\nScanning...")
+                    time.sleep(2)
+                    print("No duplicate files were found.")
+                else:
+                    print("\nScanning...")
+                    time.sleep(4)
+                    print("\nDuplicate files found:")
+                    for files in duplicate_files:
+                        for file_path in files:
+                            print(file_path)
+
+                    confirmation = input("\nDo you want to delete these duplicate files (yes/no)?\n").strip().lower()
+
+                    if confirmation == 'yes':
+                        print("\nFile deletion in process...")
+                        time.sleep(5)
+                        delete_duplicates(duplicate_files)
+                        print("Duplicate files were successfully deleted.")
+                        another_duplication = input("\nDo you want to find other duplicate files in your system (yes/no)?\n").strip().lower()
+                        if another_duplication == 'yes':
+                            continue
+                        elif another_duplication == 'no':
+                            print('\nOkay, Thank you for using our Annihilator.\n\n\n')
+                            break
+                    elif confirmation == 'no':
+                        print("\nOkay, Thank you for using our Annihilator.\n\n\n")
+                        break
+        break
