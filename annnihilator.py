1277c06c322324c4ad98ef7f53cfcc5aef8f657b
Bluishhh <145332477+bluishhhxd@users.noreply.github.com>
2023-11-22 16:26:45 +0530

    Update annnihilator.py

--- annnihilator.py
+++ annnihilator.py
@@ -2,13 +2,36 @@ import os
 import hashlib
 from collections import defaultdict
 import time
+from colorama import init, Fore
 
+init(autoreset=True)
+
+def print_message(message, color=Fore.RESET):
+    print(f"{color}{message}{Fore.RESET}")
+
+def print_red(text):
+    print_message(text, Fore.RED)
+
+def print_green(text):
+    print_message(text, Fore.GREEN)
+
+def print_yellow(text):
+    print_message(text, Fore.YELLOW)
+
+def print_cyan(text):
+    print_message(text, Fore.CYAN)
+
+def print_magenta(text):
+    print_message(text, Fore.MAGENTA)
+
+def print_BLUE(text):
+    print_message(text, Fore.BLUE)
 
 def compute_hash(file_path):
     hasher = hashlib.md5()
     with open(file_path, 'rb') as file:
         while True:
-            data = file.read(65536)  # Read the file in 64KB chunks
+            data = file.read(65536)
             if not data:
                 break
             hasher.update(data)
@@ -39,59 +62,74 @@ def delete_duplicates(duplicate_files):
 
 
 if __name__ == "__main__":
-    print("\n\nWelcome to project ANNIHILATE.\nIt is specifically designed to delete duplicate files from your Pc/laptop.\nEnjoy your free space :)\n\n\n1.) Scan: Scans for duplicate files in the provided directory path.\n2.) Quit: Quits the program")
-    while True: 
-        button = input("\nType your choice:\n").replace(" ",'')
+    print_magenta("\n\nWelcome to project ANNIHILATE.\n"
+          "It is specifically designed to delete duplicate files from your Pc/laptop.\n"
+          "Enjoy your free space :)\n\n\n"
+          "1.) Scan: Scans for duplicate files in the provided directory path.\n"
+          "2.) Quit: Quits the program")
+
+    while True:
+        button = input("\nType your choice:\n").replace(" ", '')
         if button.lower() == 'scan':
-            pass
+            while True:
+                directory_to_scan = input("\nEnter the directory path to scan for duplicate files: ")
+
+                if directory_to_scan.capitalize() == "Quit":
+                    print_cyan("\n\nOkay, Thank you for using Annihilator.\n\n\n")
+                    break
+                elif directory_to_scan == "":
+                    print_red("\nTry again, Directory path can't be empty.")
+                elif not os.path.isdir(directory_to_scan):
+                    print_red("\nPlease enter a valid directory path.")
+                else:
+                    duplicate_files = find_duplicate_files(directory_to_scan)
+
+                    if not duplicate_files:
+                        print_yellow("\nScanning...")
+                        time.sleep(2)
+                        print_green("No duplicate files were found.")
+                        other_duplication = input("\nDo you want to scan for duplicate files in other directories (yes/no)?\n").strip().lower()
+                        if other_duplication == 'yes':
+                            continue
+                        elif other_duplication == 'no':
+                            print_cyan('\nOkay, Thank you for using Annihilator.\n\n\n')
+                            break
+                    else:
+                        print_yellow("\nScanning...")
+                        time.sleep(4)
+                        print_cyan("\nDuplicate files found:")
+                        for files in duplicate_files:
+                            for file_path in files:
+                                print_message(file_path, Fore.YELLOW)
+
+                        confirmation = input("\nDo you want to delete these duplicate files (yes/no)?\n").strip().lower()
+
+                        if confirmation == 'yes':
+                            print_yellow("\nFile deletion in process...")
+                            time.sleep(5)
+                            delete_duplicates(duplicate_files)
+                            print_green("Duplicate files were successfully deleted.")
+                            another_duplication = input("\nDo you want to find other duplicate files in your system (yes/no)?\n").strip().lower()
+                            if another_duplication == 'yes':
+                                continue
+                            elif another_duplication == 'no':
+                                print_cyan('\nOkay, Thank you for using Annihilator.\n\n\n')
+                                break
+                        elif confirmation == 'no':
+                            other_duplication = input("\nDo you want to find other duplicate files in your system (yes/no)?\n").strip().lower()
+                            if other_duplication == 'yes':
+                                continue
+                            elif other_duplication == 'no':
+                                print_cyan('\nOkay, Thank you for using Annihilator.\n\n\n')
+                                break
+                            
         elif button.lower() == 'quit':
-            print("\nThank you for using our Annihilator.\n\n\n")
+            print_cyan("\nThank you for using Annihilator.\n\n\n")
             break
         elif button == '':
-            print("\nYou can't leave it empty\nIf you wish to exit the program type" + ' "quit".')
+            print_red("\nYou can't leave it empty\nIf you wish to exit the program type" + ' "quit".')
             continue
         else:
-            print("\nInvalid option, Try again.")
+            print_red("\nInvalid option, Try again.")
             continue
-        while True:
-            directory_to_scan = input("\nEnter the directory path to scan for duplicate files: ")
-
-            if directory_to_scan.capitalize() == "Quit" :
-                print("\n\nOkay, Thank you for using our Annihilator.\n\n\n")
-                break
-            elif directory_to_scan == "" :
-                print("\nTry again, Directory path can't be empty.")
-            elif not os.path.isdir(directory_to_scan):
-                print("\nPlease enter a valid directory path.")
-            else:
-                duplicate_files = find_duplicate_files(directory_to_scan)
-
-                if not duplicate_files:
-                    print("\nScanning...")
-                    time.sleep(2)
-                    print("No duplicate files were found.")
-                else:
-                    print("\nScanning...")
-                    time.sleep(4)
-                    print("\nDuplicate files found:")
-                    for files in duplicate_files:
-                        for file_path in files:
-                            print(file_path)
-
-                    confirmation = input("\nDo you want to delete these duplicate files (yes/no)?\n").strip().lower()
-
-                    if confirmation == 'yes':
-                        print("\nFile deletion in process...")
-                        time.sleep(5)
-                        delete_duplicates(duplicate_files)
-                        print("Duplicate files were successfully deleted.")
-                        another_duplication = input("\nDo you want to find other duplicate files in your system (yes/no)?\n").strip().lower()
-                        if another_duplication == 'yes':
-                            continue
-                        elif another_duplication == 'no':
-                            print('\nOkay, Thank you for using our Annihilator.\n\n\n')
-                            break
-                    elif confirmation == 'no':
-                        print("\nOkay, Thank you for using our Annihilator.\n\n\n")
-                        break
-        break
+        break 
